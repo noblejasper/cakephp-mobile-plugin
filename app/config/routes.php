0af5a66a537cb56005bd8e9ad4a79dd1806781db
noblejasper <nobjas@gmail.com>
2010-03-09 21:13:31 +0900

    add default routing

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,11 +1,14 @@
 <?php
 Router::connectNamed(array(), array('argSeparator' => '~'));
 
-/* root_controller.php を作ろう */
+// mobile only のroutingはここに書きます
+Router::connect('/m/copyright', array('controller' => 'copyright', 'action' => 'index', 'prefix' => 'mobile'));
+
+// root_controller.php を作ろう
 Router::connect('/', array('controller' => 'root', 'action' => 'index'));
 Router::connect('/m', array('controller' => 'root', 'action' => 'index', 'prefix' => 'mobile'));
 
-/* PC向けのControllerは全部書かないとまずいかも */
+// PC向けのControllerは全部書かないとまずいかも
 Router::connect('/users/:action', array('controller' => 'users'));
 // Router::connect('/pccontroller/:action', array('controller' => 'pccontroller'));
 
